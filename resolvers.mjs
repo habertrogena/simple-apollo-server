d16a09a15bf282d69ecf66d08c5c9068b4f4010b
habertrogena <habertorogy12@gmail.com>
2023-05-04 10:57:20 +0300

    added query

--- resolvers.mjs
+++ resolvers.mjs
@@ -1,7 +1,7 @@
 export const resolvers = {
     Query: {
         checkApiStatus:()=>{
-            return {status:'the api is working'}
+            return {status:'the api is working correctly'}
         }
     },
 
